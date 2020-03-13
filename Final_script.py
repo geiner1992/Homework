cb6dd1ddd2d74cab60c788db44bc3c9f11580be9
geiner1992 <geiner.miranda92@gmail.com>
2020-03-13 20:45:18 +0000

    Final_script!

--- Final_script.py
+++ Final_script.py
@@ -16,27 +16,47 @@ class Calls_APIs:
         
     def pull_cards(self):
         global new_number
+        attemps = 3
         number_cards = int(self.num_cards1)
-        if number_cards > 0:
-            banner1 = ('Please, enter how many cards you want to pull from your deck: ')
-            print(banner1)
-            time.sleep(2)
-            print(f"(Remember that you have {number_cards} cards!)")
-            time.sleep(2)
-            pulled_card = input("Enter the number here: ")
-            new_number = pulled_card
-            self.cards_left()
-        else:
-            print("You don't have more cards. The game has ended!")
-            time.sleep(2)
-            sys.exit()
+        while attemps > 0:
+            if number_cards > 0:
+                try:
+                    banner1 = ('Please, enter how many cards you want to pull from your deck: ')
+                    print(banner1)
+                    time.sleep(2)
+                    print(f"(Remember that you have {number_cards} cards!)")
+                    time.sleep(2)
+                    pulled_card = int(input("Enter the number here: "))
+                    if pulled_card <= number_cards:
+                        new_number = pulled_card
+                        self.cards_left()
+                    elif pulled_card > number_cards:
+                        time.sleep(2)
+                        print("\nNumber higher than the number cards in your deck! Try again...\n")
+                        attemps -= 1
+                    else:
+                        print("\nYou don't have more cards. The game has ended!")
+                        time.sleep(2)
+                        sys.exit()
+                        attemps -= 1
+                except ValueError:
+                    time.sleep(2)
+                    attemps -= 1
+                    print ("\nNot a value between 0 and 52!\n")
+            elif number_cards == 0:
+                print("\nYou don't have more cards. The game has ended!")
+                time.sleep(2)
+                sys.exit()
+                        
+        print("You have run out of attemps, bye!")
+        sys.exit()
     
     def cards_left(self):
         global current_id_deck
         global new_number
         current_id_deck = str(self.current_id1)
         list_cards = []
-        new_line = (f"api/deck/{current_id_deck}/draw/?count=" + new_number)
+        new_line = (f"api/deck/{current_id_deck}/draw/?count=" + str(new_number))
         new_url = (work_url + new_line)
         payload = {}
         headers = {}
@@ -56,7 +76,6 @@ def information_deck():
        loop_script = Calls_APIs(new_number, current_id_deck)
        loop_script.pull_cards()
 
-
 def main_script():
     url = (work_url + site_ext1)
     payload = {}
@@ -72,4 +91,3 @@ if __name__ == "__main__":
     value1, value2 = main_script()
     script_values = Calls_APIs(value1, value2)
     script_values.pull_cards()
-    script_values.cards_left()
